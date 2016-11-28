61e23264ce491583956cc485db17cbe72d63cc65
Tobias Schifftner <ts@ambimax.de>
2016-11-28 11:19:02 +0100

    Upgraded mmonit to 3.6.2

--- templates/setup.sh
+++ templates/setup.sh
@@ -5,23 +5,23 @@ if [ -f /etc/.mmonit_installed ]; then
     exit 0
 fi
 
-host=http://localhost:{{ mmonit_connectors.0.port }}
+host={{ mmonit_setup_url }}
 
 # create secure cookie storage
 mkdir -p -m 0700 ~/.mmonit
 
 # go to login page to retrieve cookie
-curl -c ~/.mmonit/cookie ${host}/index.csp > /dev/null
+curl --insecure -c ~/.mmonit/cookie ${host}/index.csp # > /dev/null
 
 # login
-curl -b ~/.mmonit/cookie \
+curl --insecure -b ~/.mmonit/cookie \
  -d z_username=admin \
  -d z_password=swordfish \
  -d z_csrf_protection=off \
- ${host}/z_security_check > /dev/null
+ ${host}/z_security_check #> /dev/null
 
 # Create admin
-curl -b ~/.mmonit/cookie \
+curl --insecure -b ~/.mmonit/cookie \
  -d uname='{{ mmonit_admin_username }}' \
  -d fullname='Administrator' \
  -d password='{{ mmonit_admin_password }}' \
@@ -29,7 +29,7 @@ curl -b ~/.mmonit/cookie \
  ${host}/admin/users/create > /dev/null
 
 # Create monit user
-curl -b ~/.mmonit/cookie \
+curl --insecure -b ~/.mmonit/cookie \
  -d uname='{{ mmonit_connector_username }}' \
  -d fullname='monit' \
  -d password='{{ mmonit_connector_password }}' \
@@ -37,19 +37,19 @@ curl -b ~/.mmonit/cookie \
  ${host}/admin/users/create > /dev/null
 
 # Delete default admin
-curl -b ~/.mmonit/cookie \
+curl --insecure -b ~/.mmonit/cookie \
  -d uname=admin \
  ${host}/admin/users/delete > /dev/null
 
 # Delete default monit user
-curl -b ~/.mmonit/cookie \
+curl --insecure -b ~/.mmonit/cookie \
  -d uname=monit \
  ${host}/admin/users/delete > /dev/null
 
 # Set done if grep is successful
-curl -b ~/.mmonit/cookie ${host}/admin/users/list | grep '{{ mmonit_connector_username }}' && touch /etc/.mmonit_installed
+curl --insecure -b ~/.mmonit/cookie ${host}/admin/users/list | grep '{{ mmonit_connector_username }}' && touch /etc/.mmonit_installed
 
 # logout
-curl -b ~/.mmonit/cookie ${host}/login/logout.csp
+curl --insecure -b ~/.mmonit/cookie ${host}/login/logout.csp
 
 exit 0
