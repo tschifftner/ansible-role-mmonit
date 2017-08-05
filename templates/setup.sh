88a34e16eb3e541eabb46d6b1077bfc0646cd919
Tobias Schifftner <ts@ambimax.de>
2017-08-05 14:54:17 +0200

    Updated setup script

--- templates/setup.sh
+++ templates/setup.sh
@@ -6,19 +6,27 @@ if [ -f /etc/.mmonit_installed ]; then
 fi
 
 host={{ mmonit_setup_url }}
+logfile=/etc/mmonit_setup.log
+
+function error_exit {
+    cat $logfile
+	echo "$1" 1>&2
+	exit 1
+}
+
 
 # create secure cookie storage
-mkdir -p -m 0700 ~/.mmonit
+mkdir -p -m 0700 ~/.mmonit || error_exit "Cannot create ~/.mmonit"
 
 # go to login page to retrieve cookie
-curl --insecure -c ~/.mmonit/cookie ${host}/index.csp # > /dev/null
+curl --insecure -c ~/.mmonit/cookie ${host}/index.csp > $logfile || error_exit "Cannot retrieve login cookie"
 
 # login
 curl --insecure -b ~/.mmonit/cookie \
  -d z_username=admin \
  -d z_password=swordfish \
  -d z_csrf_protection=off \
- ${host}/z_security_check #> /dev/null
+ ${host}/z_security_check >> $logfile || error_exit "Cannot create ~/.mmonit"
 
 # Create admin
 curl --insecure -b ~/.mmonit/cookie \
@@ -26,7 +34,7 @@ curl --insecure -b ~/.mmonit/cookie \
  -d fullname='Administrator' \
  -d password='{{ mmonit_admin_password }}' \
  -d admin=1 \
- ${host}/admin/users/create > /dev/null
+ ${host}/admin/users/create >> $logfile || error_exit "Create admin failed"
 
 # Create monit user
 curl --insecure -b ~/.mmonit/cookie \
@@ -34,22 +42,25 @@ curl --insecure -b ~/.mmonit/cookie \
  -d fullname='monit' \
  -d password='{{ mmonit_connector_password }}' \
  -d admin=0 \
- ${host}/admin/users/create > /dev/null
+ ${host}/admin/users/create >> $logfile || error_exit "Cannot mmonit user failed"
 
 # Delete default admin
 curl --insecure -b ~/.mmonit/cookie \
  -d uname=admin \
- ${host}/admin/users/delete > /dev/null
+ ${host}/admin/users/delete >> $logfile || error_exit "Cannot delete default mmonit admin"
 
 # Delete default monit user
 curl --insecure -b ~/.mmonit/cookie \
  -d uname=monit \
- ${host}/admin/users/delete > /dev/null
+ ${host}/admin/users/delete >> $logfile || error_exit "Cannot delete default mmonit user"
 
-# Set done if grep is successful
-curl --insecure -b ~/.mmonit/cookie ${host}/admin/users/list | grep '{{ mmonit_connector_username }}' && touch /etc/.mmonit_installed
+# Check if configuration is successful
+curl --insecure -b ~/.mmonit/cookie ${host}/admin/users/list | grep '{{ mmonit_connector_username }}' || error_exit "Cannot retrieve login cookie"
+
+# mark installation as success
+touch /etc/.mmonit_installed || error_exit "Cannot mark installation as success"
 
 # logout
-curl --insecure -b ~/.mmonit/cookie ${host}/login/logout.csp
+curl --insecure -b ~/.mmonit/cookie ${host}/login/logout.csp || error_exit "Cannot logout"
 
 exit 0
